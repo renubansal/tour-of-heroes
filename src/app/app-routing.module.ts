70131161d8cb73c4669bf9f6a07452bf8e992df8
renu.bansal <renu.bansal@thoughtworks.com>
2020-09-23 01:00:46 +0530

    go to component using activated route and location and come back to original component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: '', redirectTo: '/dashboard', pathMatch: 'full'
    },
+   {
+    path: 'detail/:id', component: HeroDetailComponent
+   },
+
 ];
 
 @NgModule({
